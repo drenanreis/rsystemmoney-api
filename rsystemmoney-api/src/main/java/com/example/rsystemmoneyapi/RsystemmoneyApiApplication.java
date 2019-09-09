50a22472f51b3886add319a59e7f2988e40fb1ff
drena <drena@DESKTOP-71LFNFR>
2019-09-09 08:13:09 -0300

    Inicio do pojeto

---
+++ rsystemmoney-api/src/main/java/com/example/rsystemmoneyapi/RsystemmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsystemmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsystemmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsystemmoneyApiApplication.class, args);
+	}
+
+}
